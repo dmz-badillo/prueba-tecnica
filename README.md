1d179f50e4ab2f133e26bcdda3f519db5131a4dc
arely <dmz.badillo@gmail.com>
2025-11-26 23:58:30 -0600

    buenas practicas

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # prueba-tecnica
-Pasos de instalación
-1.- clonar el proyecto
-2.- Crear una base de datos en mysql llamada paqueterias
-3.- Levantar el servicio con el siguiente comando symfony server:start
-4.- Enviar la petición desde postman
-POST 127.0.0.1:8000/quotes
+======================  
+Versiones:  
+======================  
+Symfony 6.4.29   
+PHP  8.2.12  
+
+
+======================  
+Pasos de instalación:  
+======================  
+1.- clonar el proyecto  
+2.- Crear una base de datos en mysql llamada quote   
+3.- Migrar la bd con el siguiente comando   
+    php bin/console make:migration   
+
+5.- Levantar el servicio con el siguiente comando    
+    symfony server:start   
+
+6.- Enviar la petición desde postman   
+POST 127.0.0.1:8000/api/v1/quotes  
 
 {
   "originZipcode": "12345",
   "destinationZipcode": "54321"
 }
+
+
+======================   
+Patrones de diseño   
+======================   
+Strategy , se crea interface para el manejo de las paqueterias, asi al agregar una nueva solo hay que configurarla en el service.yml 
--- src/Controller/QuotesController.php
+++ src/Controller/QuotesController.php
@@ -8,9 +8,26 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * QuotesController
+ *
+ * Controlador para manejar solicitudes de cotizaciones de envío de paqueterias.
+ *
+ * @author Arely Dominguez
+ * @date 26/11/2025
+ */
 class QuotesController extends AbstractController
 {
-    #[Route('/quotes', methods: ['POST'])]
+    /**
+     * Endpoint para consultar cotizaciones de proveedores de paquetería.
+     *
+     * @param Request $request La solicitud HTTP que contiene los códigos postales en el body.
+     * @param QuotesService $service Servicio que maneja la lógica de obtención de infomación de proveedores.
+     *
+     * @return JsonResponse Respuesta en formato JSON con los proveedores y sus cotizaciones,
+     *         o un mensaje de error si falta algún parámetro.
+     */
+    #[Route('/api/v1/quotes', methods: ['POST'])]
     public function check(Request $request, QuotesService $service): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
